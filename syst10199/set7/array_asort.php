79a9e93e4e979e78509d8645b820afc9df57c3ea
Ellen Bajcar <ellen.bajcar@sheridaninstitute.ca>
2021-07-12 18:04:24 -0400

    Create array_asort.php

---
+++ syst10199/set7/array_asort.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>PHP ksort</title>
+	<meta charset="utf-8">
+</head>
+<body>
+
+<?php
+/*
+  Author: Your Instructor
+  File name: array_ksort.php
+  Date created: Summer 2020
+  Description: 
+  
+ksort() sorts on key
+asort() sorts on value
+
+www.php.net/manual/en/array.sorting.php
+*/
+$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+asort($age);
+
+foreach($age as $x => $x_value) {
+   echo "Key=" . $x . ", Value=" . $x_value;
+   echo "<br>";
+}
+?>
+
+</body>
+</html>
