4fef949e25aafc71f022fe0457be93910da5697b
Ellen Bajcar <ellen.bajcar@sheridaninstitute.ca>
2023-07-02 06:46:55 -0400

    Create using_here_doc.php

---
+++ examples/set6/using_here_doc.php
@@ -0,0 +1,34 @@
+<?php 
+/*
+Using here document
+same as bash shell input redirection
+
+Create valid HTML code
+*/
+
+print <<<TAG1
+<!DOCTYPE html>
+<html>
+<head>
+	<title>here doc and input redirection</title>
+	<meta charset="utf-8">
+</head>
+<body>
+  <header><h1>Page header</h1></header>
+  <nav>
+    <a href="#">Link1</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+    <a href="#">Link2</a>
+  </nav>
+  <main>
+    <h3>Content heading</h3>
+    <p>Content &hellip;</p>
+  </main>
+  <footer>
+    <address>Web Programming &copy; Sheridan College</address>
+  </footer>
+</body>
+</html>
+TAG1;
+
+
+?>
